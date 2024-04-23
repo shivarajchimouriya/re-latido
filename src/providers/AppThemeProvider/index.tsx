14733f3c5318d047fe4e65945ec8818693070544
ankitbhusal20 <ankit@bottle.com.np>
2024-04-23 21:05:48 +0545

    linitng fixes

--- src/providers/AppThemeProvider/index.tsx
+++ src/providers/AppThemeProvider/index.tsx
@@ -1,18 +1,19 @@
-"use client";
-import React, { ReactNode } from "react";
-import { ChakraBaseProvider } from "@chakra-ui/react";
-import { theme } from "@/theme";
+'use client';
+
+import React, { ReactNode } from 'react';
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import { theme } from '@/theme';
 
 interface IProps {
   children: ReactNode;
 }
 
-const AppThemeProvider = ({ children }: IProps) => {
+function AppThemeProvider({ children }: IProps) {
   return (
     <ChakraBaseProvider theme={theme}>
       {children}
     </ChakraBaseProvider>
   );
-};
+}
 
 export default AppThemeProvider;
