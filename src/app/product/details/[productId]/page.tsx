176408a54787a20321e767a540c405f28a4da5be
ankitbhusal20 <ankit@bottle.com.np>
2024-04-23 21:00:44 +0545

    wrap the product detail page with context

--- src/app/product/details/[productId]/page.tsx
+++ src/app/product/details/[productId]/page.tsx
@@ -1,9 +1,12 @@
-import ProductDetailPage from '@/features/ProductDetailPage/ProductDetailPage';
+import ProductDetailPage from '@/features/ProductDetailPage';
 import React from 'react';
+import { ProductDetailProvider } from '@/contexts/ProductDetailContext';
 import IProps from './IProps';
 
 export default function ProductDetail({ params }: IProps) {
   return (
-    <ProductDetailPage productId={params.productId} />
+    <ProductDetailProvider>
+      <ProductDetailPage productId={params.productId} />
+    </ProductDetailProvider>
   );
 }
