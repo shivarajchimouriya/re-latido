5b24b9e02df0424fe107c13381ce20153cb4924f
ankitbhusal20 <ankit@bottle.com.np>
2024-04-25 18:37:13 +0545

    registered secondary text color to theme context

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -3,6 +3,7 @@ export const appColor = {
     border: "var(--border)",
     base: "var(--base)",
     textPrimary: "var(--text-primary)",
+    textSecondary: "var(--text-secondary)",
     black: "var(--black)",
     background: "var(--background)"
 }
