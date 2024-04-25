fed84b1eb065fd0bb894fd92ae9dec2c1616b206
ankitbhusal20 <ankit@bottle.com.np>
2024-04-25 10:51:44 +0545

    blog wraper

--- src/features/ProductDetailPage/Collections/components/BlogWrapper/index.tsx
+++ src/features/ProductDetailPage/Collections/components/BlogWrapper/index.tsx
@@ -1,34 +1,39 @@
-import React from 'react'
-import { TODO } from '../../../../../../global'
-import { Box } from '@chakra-ui/react';
-import AppImage from '@/components/AppImage';
+import React from "react";
+import { TODO } from "../../../../../../global";
+import { Box, Grid, Text } from "@chakra-ui/react";
+import AppImage from "@/components/AppImage";
+import PostBuilder from "../PostBuilder";
+import parse from "html-react-parser";
 
-export default function BlogWrapper({ data, primaryMedia }: TODO) {
+interface IBlogWrapper {
+  data: TODO;
+  primaryMedia: string;
+}
+
+export default function BlogWrapper({ data, primaryMedia }: IBlogWrapper) {
   return (
-    <Box width={'100%'}>
+    <Box width={"100%"}>
       {primaryMedia && (
-        <div className="main__image__container">
-          <AppImage height={400} width={400} alt={'banner'} src={primaryMedia} />
-        </div>
+        <Box my={"1rem"}>
+          <AppImage
+            height={500}
+            width={500}
+            alt={"banner"}
+            src={primaryMedia}
+          />
+        </Box>
       )}
-      {/* // {data &&
-      //   data?.map((i: any, n: any) => {
-      //     if (Array.isArray(i)) {
-      //       return <PostBuilder key={uuidv4()} data={i} />;
-      //     }
-      //     if (typeof i === 'object') {
-      //       const p = i.playlist.map((i: any) => ({
-      //         src: i,
-      //         title: data?.title,
-      //         author: data?.author,
-      //         time: 0,
-      //         duration: 0,
-      //       }));
-      //       return <PlayBar playlist={p} key={n} />;
-      //     }
-      //     return parse(i);
-      //   })}
-    // </div> */}
-  </Box>
-  )
+      {data &&
+        data?.map((i: any) => {
+          if (Array.isArray(i)) {
+            return <PostBuilder data={i} />;
+          }
+          return (
+            <Box my="2rem" fontSize={"1.4rem"}>
+              {parse(i)}
+            </Box>
+          );
+        })}
+    </Box>
+  );
 }
