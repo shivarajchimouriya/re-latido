feb88456d23082623528ff47fc6b83b68a353236
ankitbhusal20 <ankit@bottle.com.np>
2024-04-25 18:37:39 +0545

    mock profile data

---
+++ src/data/mock/profileData/index.ts
@@ -0,0 +1,17 @@
+export const profileData = {
+  data: {
+    _id: "65a50cfff86faf67a5403212",
+    email: "ankitbhusal59@gmail.com",
+    DOB: "2000-09-21T00:00:00.000Z",
+    __v: 0,
+    createdAt: "2024-01-15T10:46:23.675Z",
+    gender: "Male",
+    name: "Ankit Bhusal",
+    phone: "9824408587",
+    updatedAt: "2024-04-25T11:41:32.953Z",
+    web_client: true,
+    profile_image:
+      "https://l-inventory-test-vikash.s3.ap-south-1.amazonaws.com/public/profile/userProfile/1714045290495",
+    address: "Kawasoti, Nawalpur",
+  },
+};
