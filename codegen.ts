bc92f3581429ed55e1e5ea616eb950527df0f706
ankitbhusal20 <ankit@bottle.com.np>
2024-05-23 10:44:44 +0545

    graphql

---
+++ codegen.ts
@@ -0,0 +1,23 @@
+import environment from "@/config/environment";
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "https://sizemodule-api.uat.latido.com.np/graphql",
+  documents: ["src/GraphQl/**/*.ts"],
+  generates: {
+    "./src/GraphQl/Generated/graphql.tsx": {
+      presetConfig: {
+        gqlTagName: "gql",
+
+      },
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+        "fragment-matcher",
+      ],
+    },
+  },
+};
+
+export default config;
