4bb7e55a735402f91d878ebba07cb669b08a7d92
ankitbhusal20 <ankit@bottle.com.np>
2024-05-27 17:47:41 +0545

    size recommendation not found

---
+++ src/features/ProductDetailPage/Collections/components/SizeRecommendationNotFound/index.tsx
@@ -0,0 +1,25 @@
+import { socialUrls } from "@/constants/links";
+import { appColor } from "@/theme/foundations/colors";
+import { Box, Icon, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { MdOutlineWhatsapp } from "react-icons/md";
+
+export default function SizeRecommendationNotFound() {
+  return (
+    <Box my={8} mx={8} color={appColor.base} fontSize={"1.4rem"}>
+      <Text textAlign={"center"}>
+        Can't find your size? No worries! Reach out to our Customer Care on
+        <Link href={socialUrls.whatsapp} target="_blank">
+          <Icon
+            mx={"0.5rem"}
+            fontSize={"2.4rem"}
+            as={MdOutlineWhatsapp}
+            mb={-2}
+            color="whatsapp"
+          />
+        </Link>
+      </Text>
+    </Box>
+  );
+}
