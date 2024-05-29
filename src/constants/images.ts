3ab3bb2aef38fe70e80dc7ef60314251f098436e
ankitbhusal20 <ankit@bottle.com.np>
2024-05-29 16:42:36 +0545

    invoice finished

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,18 +1,16 @@
 export const collectionImages = {
-    latido: "/images/collections/latido.jpeg",
-    ka: "/images/collections/ka.png",
-    kha: "/images/collections/kha.png",
-    ga: "/images/collections/ga.png",
-    gha: "/images/collections/gha.png",
-    na: "/images/collections/na.png",
-    cha: "/images/collections/cha.png"
+  latido: "/images/collections/latido.jpeg",
+  ka: "/images/collections/ka.png",
+  kha: "/images/collections/kha.png",
+  ga: "/images/collections/ga.png",
+  gha: "/images/collections/gha.png",
+  na: "/images/collections/na.png",
+  cha: "/images/collections/cha.png",
+};
 
-}
-
-export const brandLogo = '/'
-
-
+export const brandLogo = "/";
+export const brandLogoWide = "/icons/latido-wide.png";
 
 export const jacketImages = {
-    sheep: '/images/jackets/jacket.avif'
-}
+  sheep: "/images/jackets/jacket.avif",
+};
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,28 +1,24 @@
-import 'dayjs/plugin/utc'; // Import the UTC plugin for dayjs
+import "dayjs/plugin/utc"; // Import the UTC plugin for dayjs
 import dayjs from "dayjs";
 
 export const dateToMonthAndDay = (date: string) => {
-    const dateToFormat = dayjs(date);
-    const formattedDate = dateToFormat.format('MMM D');
-    return formattedDate;
-}
+  const dateToFormat = dayjs(date);
+  const formattedDate = dateToFormat.format("MMM D");
+  return formattedDate;
+};
 
 export const dateToMonthDayYear = (date: string) => {
-    const dateToFormat = dayjs(date);
-    const formattedDate = dateToFormat.format('MMM D, YYYY');
-    return formattedDate;
-}
-
-
+  const dateToFormat = dayjs(date);
+  const formattedDate = dateToFormat.format("MMM D, YYYY");
+  return formattedDate;
+};
 
 export const formatDateForSearch = (incomingDate: Date) => {
-    const date = dayjs(incomingDate);
-    const formattedDate = date.format('YYYY-MM-DD');
-    return formattedDate
-}
+  const date = dayjs(incomingDate);
+  const formattedDate = date.format("YYYY-MM-DD");
+  return formattedDate;
+};
 
 export const isDateVaild = (incomingDate: string) => {
-
-    return dayjs(incomingDate).isValid()
-
-}
+  return dayjs(incomingDate).isValid();
+};
