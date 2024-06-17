82b358ec4023c04bd44ad00cd085098cbdd7a333
ankitbhusal20 <ankit@bottle.com.np>
2024-06-17 15:28:52 +0545

    safe orientation added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ import Script from "next/script";
 import { Box } from "@chakra-ui/react";
 import Header from "@/components/Layout/PrimaryLayout/components/Header";
 import JsonLd from "@/features/JsonLd";
+import SafeOrientation from "@/features/SafeOrientation";
 const APP_NAME = "Latido";
 const APP_DEFAULT_TITLE = "Latido";
 const APP_TITLE_TEMPLATE = "Latido";
@@ -77,28 +78,30 @@ export default function RootLayout({
           overflow: "hidden",
         }}
       >
-        <div
-          id="rootContainer"
-          style={{
-            overflow: "auto",
-            position: "relative",
-            scrollSnapType: "y mandatory",
-            scrollSnapStop: "always",
-            scrollBehavior: "smooth",
-            height: "100dvh",
-            scrollPaddingTop: "4.6rem"
-          }}
-        >
-          <NextTopLoader color="red" showSpinner={false} />
-          <AuthProvider>
-            <AppQueryProvider>
-              <AppThemeProvider>
-                <Header />
-                {children}
-              </AppThemeProvider>
-            </AppQueryProvider>
-          </AuthProvider>
-        </div>
+        <SafeOrientation>
+          <div
+            id="rootContainer"
+            style={{
+              overflow: "auto",
+              position: "relative",
+              scrollSnapType: "y mandatory",
+              scrollSnapStop: "always",
+              scrollBehavior: "smooth",
+              height: "100dvh",
+              scrollPaddingTop: "4.6rem",
+            }}
+          >
+            <NextTopLoader color="red" showSpinner={false} />
+            <AuthProvider>
+              <AppQueryProvider>
+                <AppThemeProvider>
+                  <Header />
+                  {children}
+                </AppThemeProvider>
+              </AppQueryProvider>
+            </AuthProvider>
+          </div>
+        </SafeOrientation>
       </body>
     </html>
   );
