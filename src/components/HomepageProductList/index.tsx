f20c155180d990292ec35b9fbd5ee60b555ff861
ankitbhusal20 <ankit@bottle.com.np>
2024-07-02 13:19:37 +0545

    type changed

--- src/components/HomepageProductList/index.tsx
+++ src/components/HomepageProductList/index.tsx
@@ -4,7 +4,7 @@ import { IProduct } from "@/resources/Product/interface";
 import { Box } from "@chakra-ui/react";
 
 interface IProps {
-  products: any[];
+  products: IProduct[];
   viewType: 1;
 }
 
