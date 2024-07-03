763b2341af55b750cfd3b2aa6ab8b55a6fb52147
ankitbhusal20 <ankit@bottle.com.np>
2024-07-03 13:51:35 +0545

    data not found page and category product list from gender

--- src/components/HomepageProductList/index.tsx
+++ src/components/HomepageProductList/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductList1 from "./ProductList1";
 import { IProduct } from "@/resources/Product/interface";
-import { Box } from "@chakra-ui/react";
+import { Box, Grid, Text } from "@chakra-ui/react";
 
 interface IProps {
   products: IProduct[];
@@ -13,6 +13,11 @@ const HomepageProductLists = ({ products, viewType }: IProps) => {
     1: (product: IProduct[]) => (
       <Box mt="15dvh">
         <ProductList1 products={product} />
+        <Grid width="full" mt="2rem" minH="80vh" justifyContent="center">
+          <Text fontSize="2rem" fontWeight="bold">
+            End of Product
+          </Text>
+        </Grid>
       </Box>
     ),
   };
