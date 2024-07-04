881041f309abdf87799f14fa76c55547f0e005e9
ankitbhusal20 <ankit@bottle.com.np>
2024-07-04 14:42:31 +0545

    3d modal

---
+++ src/components/Cards/3dCard/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Center, OrbitControls, useGLTF } from "@react-three/drei";
+
+function Model() {
+  const { scene } = useGLTF("/3d/leather.glb");
+  return <primitive object={scene} />;
+}
+
+export default function ThreeDCard() {
+  return (
+    <Canvas>
+      <ambientLight intensity={1} />
+      <Suspense fallback={null}>
+        <Center >
+          <Model />
+        </Center>
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+}
