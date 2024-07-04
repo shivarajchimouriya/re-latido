e81d8c6870fba6cfd3d54c6a34f7061a7b5da595
ankitbhusal20 <ankit@bottle.com.np>
2024-07-04 11:54:26 +0545

    spotify playlist embaded

--- src/components/Cards/ProductCard/index.tsx
+++ src/components/Cards/ProductCard/index.tsx
@@ -272,7 +272,7 @@ const ProductCard = ({ product, isFirstCard, ...rest }: IProps) => {
             transitionDuration=".4s"
             align="center"
             rounded="1rem"
-            roundedBottom= "2rem"
+            roundedBottom="2rem"
             overflow="hidden"
             bg="rgba(0,0,0,0.03)"
             // onContextMenu={(event) => {
