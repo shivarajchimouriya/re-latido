92b1d20ea6f5dd81c04c09a474e6c4a26e904ccf
ankitbhusal20 <ankit@bottle.com.np>
2024-08-02 22:26:15 +0545

    try catch added

--- src/app/api/test/success/route.ts
+++ src/app/api/test/success/route.ts
@@ -1,12 +1,21 @@
+import { logger } from "@/utils/logger";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const data = (await req.formData()).get("xmlmsg");
+  try {
+    const data = (await req.formData()).get("xmlmsg");
 
-  return NextResponse.json(
-    {
-      data: data,
-    },
-    { status: 200 }
-  );
+    return NextResponse.json(
+      {
+        data: data,
+      },
+      { status: 200 }
+    );
+
+  }
+  catch (err) {
+    logger.log("Errr", err)
+
+  }
+
 }
