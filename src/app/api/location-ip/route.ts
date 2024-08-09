839bfd26dcd15b016b338d47164d46426afd26e1
ankitbhusal20 <ankit@bottle.com.np>
2024-08-09 12:39:37 +0545

    include error in response

--- src/app/api/location-ip/route.ts
+++ src/app/api/location-ip/route.ts
@@ -40,6 +40,6 @@ export async function POST(req: NextRequest) {
 
     } catch (error) {
         logger.error("Error fetching API: ", error);
-        return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+        return NextResponse.json({ message: "Something went wrong", error: error }, { status: 500 });
     }
 }
